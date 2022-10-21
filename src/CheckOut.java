0c2a7e23b80977a94bb3c40489b8cf22de3b81ef
JacobDorow <dorowjm@mail.uc.edu>
2022-10-20 22:48:57 -0400

    Initial commit

---
+++ src/CheckOut.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class CheckOut {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double itemPrice;
+        boolean continueOn;
+        boolean done = true;
+        double sum = 0;
+        do {
+            itemPrice = SafeInput.getRangedDouble(in, "Enter the price of the item", .50, 9.99);
+            continueOn = SafeInput.getYNConfirm(in, "Do you have another item?");
+            if (continueOn) {
+                itemPrice = in.nextDouble();
+                sum += itemPrice;
+            } else {
+                done = false;
+            }
+
+        }while (done);
+        System.out.println(sum);
+    }
+}
